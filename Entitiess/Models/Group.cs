bada0672588ef78ced1750c1d1b5f2f97cf5ab79
dbBurzum <khalidsr@code.edu.az>
2022-04-12 16:38:25 +0400

    Add project files.

---
+++ Entitiess/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using Entitiess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitiess.Models
+{
+    public class Group:IEntity
+    {
+     
+        public string Name { get; set; }
+        public int MaxSize { get; set; }
+        public int Id { get ; set ; }
+    }
+}
---
+++ Entitiess/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using Entitiess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entitiess.Models
+{
+    public class Student:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
